04dbf60a80cb418f5b167874150f149736f39395
Ibrahim Aslan <i.aslan2020@gtu.edu.tr>
2024-08-12 16:15:13 +0300

    Add name selection

---
+++ NetcodeTest/Assets/Scripts/UI/NameSelector.cs
@@ -0,0 +1,41 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace NetcodeTest.UI
+{
+    public class NameSelector : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField nameInputField;
+        [SerializeField] private Button connectButton;
+        [SerializeField] private int minNameLength = 1;
+        [SerializeField] private int maxNameLength = 12;
+
+        private const string PLAYER_NAME_KEY = "PlayerName";
+        
+        private void Start()
+        {
+            if (SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Null)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                return;
+            }
+            
+            nameInputField.text = PlayerPrefs.GetString(PLAYER_NAME_KEY, string.Empty);
+            HandleNameChanged();
+        }
+
+        public void HandleNameChanged()
+        {
+            connectButton.interactable = nameInputField.text.Length >= minNameLength && nameInputField.text.Length <= maxNameLength;
+        }
+
+        public void Connect()
+        {
+            PlayerPrefs.SetString(PLAYER_NAME_KEY, nameInputField.text);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
